f929ccbf926127608760efed4d762d78a42d1d9c
Felipe Z Saldanha <fzsaldanha@hotmail.com>
2020-07-08 22:51:23 -0300

    create class Tasklist and constructor method

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("teste");
+class TaskList {
+    constructor() {
+        this.titleInput = document.getElementById("messageTitle");
+        this.messageInput = document.getElementById("messageBody");
+        this.addButton = document.getElementById("addButton");
+        this.scrapsField  =document.getElementById("scrapsField");
+
+        this.scraps = [];
+
+        
+        
+    }
+}
