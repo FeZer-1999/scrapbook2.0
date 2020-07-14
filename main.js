002c8960b151ac3ab6fc08be5d9fc9f1d187073d
Felipe Z Saldanha <fzsaldanha@hotmail.com>
2020-07-13 21:05:45 -0300

    deleteScraps method, create generate id

--- main.js
+++ main.js
@@ -1,13 +1,87 @@
 class TaskList {
-    constructor() {
-        this.titleInput = document.getElementById("messageTitle");
-        this.messageInput = document.getElementById("messageBody");
-        this.addButton = document.getElementById("addButton");
-        this.scrapsField  =document.getElementById("scrapsField");
+  constructor() {
+    this.titleInput = document.getElementById("messageTitle");
+    this.messageInput = document.getElementById("messageBody");
+    this.addButton = document.getElementById("addButton");
+    this.scrapsField  =document.getElementById("scrapsField");
 
-        this.scraps = [];
+    this.scraps = [];
 
-        
-        
+    this.setAddButtonEvent();
+  }
+
+  generateScrapId() {
+    return this.scraps.length + 1;
+  }
+
+  setAddButtonEvent() {
+    this.addButton.onclick = () => this.addNewScrap();
+  }
+
+  setButtonEvents() {
+    document.querySelectorAll(".delete-button").forEach((item) => {
+      item.onclick = (event) => this.deleteScraps(event);
+    });
+  }
+
+  renderScraps() {
+    this.scrapsField.innerHTML = "";
+      
+    for (const scrap of this.scraps) {
+      const cardHtml = this.createScrapCard(scrap.title, scrap.message);
+    
+      this.insertHtml(cardHtml);
     }
+    this.setButtonEvents();
+  }
+  
+  addNewScrap() {
+    let title = this.titleInput.value;
+    let message = this.messageInput.value;
+     
+    if (!messageTitle.value || !messageBody.value) {
+      return alert("Preencha todos os campos")
+    }
+
+    const id = this.generateScrapId();
+      
+    this.titleInput.value = "";
+    this.messageInput.value = "";
+        
+    this.scraps.push({ id, title, message });
+      
+    this.renderScraps();
+         
+  }
+
+  deleteScraps(event){
+    event.path[2].remove();
+  }
+
+  insertHtml(html) {
+    this.scrapsField += html;
+  }
+
+  createScrapCard(title, message) {
+    return `
+      <div class="message-cards card text-white bg-dark m-2">
+        <div class="card-header font-weight-bold">${title}</div>
+        <div class="card-body">
+          <p class="card-text">
+            ${message}
+          </p>
+        </div>
+        <div class="w100 d-flex justify-content-end pr-2 pb-2">
+          <button class="btn btn-danger mr-1 delete-button">
+            Deletar
+          </button>
+          <button class="btn btn-info">
+            Editar
+          </button>
+        </div>
+      </div>
+    `;
+  }
 }
+
+new TaskList();
