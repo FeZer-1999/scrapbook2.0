c5e4827ab1dcf6787cf000b30eab04134657050b
Felipe Z Saldanha <fzsaldanha@hotmail.com>
2020-08-17 22:31:23 -0300

    add scrapId in saveChanges

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ class TaskList {
 
     await api.put(`/recados/${scrapId}`, {title, message});
     
-    this.scraps[scrapIndex] = { title, message };
+    this.scraps[scrapIndex] = {scrapId, title, message };
     
     this.renderScraps();
     $("#editModal").modal("hide");
